c0764c1f31939e30e055083d9806c5c05a84a2e8
benjamin-schultz <41674946+benjamin-schultz@users.noreply.github.com>
2020-11-24 17:29:36 +1100

    Added UPnP
    added UPnP so now can access via internet

--- ESP/COWS/COWS.ino
+++ ESP/COWS/COWS.ino
@@ -1,4 +1,7 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <TinyUPnP.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 
 //needed for library
 #include <DNSServer.h>
@@ -7,11 +10,16 @@
 
 #include <uri/UriBraces.h>
 
-ESP8266WebServer server(80);
-
+#define LISTEN_PORT 6665
+#define LEASE_DURATION 36000
+#define FRIENDLY_NAME "COWS"
 #define MOTOR_PIN 0
 
+TinyUPnP tinyUPnP(20000);
+ESP8266WebServer server(LISTEN_PORT);
+
 void handleDuration() {
+  
   int duration = server.pathArg(0).toInt();
   server.send(200, "text/plain", "Watering for " + String(duration) + " seconds");
   digitalWrite(MOTOR_PIN, LOW);
@@ -19,24 +27,44 @@ void handleDuration() {
   digitalWrite(MOTOR_PIN, HIGH);
 }
 
+void setupUPnP() {
+  portMappingResult portMappingAdded;
+  tinyUPnP.addPortMappingConfig(WiFi.localIP(), LISTEN_PORT, RULE_PROTOCOL_TCP, LEASE_DURATION, FRIENDLY_NAME);
+  while (portMappingAdded != SUCCESS && portMappingAdded != ALREADY_MAPPED) {
+    portMappingAdded = tinyUPnP.commitPortMappings();
+    Serial.println("");
+
+    if (portMappingAdded != SUCCESS && portMappingAdded != ALREADY_MAPPED) {
+      tinyUPnP.printAllPortMappings();
+      Serial.println(F("This was printed because adding the required port mapping failed"));
+      delay(30000);
+    }
+  }
+
+  Serial.println("UPnP done");
+}
+
 void setup() {
 
-  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, INPUT);
 
   Serial.begin(115200);
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  wifiManager.resetSettings();
+  delay(5);
+  if (MOTOR_PIN == 0) { 
+    wifiManager.resetSettings();
+  }
 
+  pinMode(MOTOR_PIN, OUTPUT);
+  
   IPAddress _ip = IPAddress(192, 168, 5, 20);
-  IPAddress _gw = IPAddress(192, 168, 86, 1);
+  IPAddress _gw = IPAddress(192, 168, 5, 1);
   IPAddress _sn = IPAddress(255, 255, 255, 0);
 
   wifiManager.setAPStaticIPConfig(_ip, _gw, _sn);
-
-  wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
 
   //fetches ssid and pass from eeprom and tries to connect
   //if it does not connect it starts an access point with the specified name
@@ -44,6 +72,14 @@ void setup() {
 
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
+
+  Serial.println(WiFi.localIP());
+
+  setupUPnP();
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println(F("MDNS responder started"));
+  }
 
   server.on(UriBraces("/duration={}"), handleDuration);
 
